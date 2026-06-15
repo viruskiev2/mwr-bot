8018cd4742d51aa5f0265f751b663ee61e11bd27
Ihor Boyko <viruksiev2@gmail.com>
2026-06-15 00:40:17 +0000

    use llama-3.1-70b-versatile

--- mwr_bot_1.py
+++ mwr_bot_1.py
@@ -91,7 +91,7 @@ async def ask_ai(user_id, user_text):
                 "Content-Type": "application/json"
             },
             json={
-                "model": "mixtral-8x7b-32768",
+                "model": "llama-3.1-70b-versatile",
                 "messages": messages,
                 "temperature": 0.7,
                 "max_tokens": 1000
